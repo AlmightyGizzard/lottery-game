3ed2efdcada4df40570427710acd930605e53036
Dominic (Laptop) <dominicohen+1@gmail.com>
2023-09-09 14:49:35 +0100

    GSAP Animations
    Added animations

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,33 +1,34 @@
-import { Container, Sprite, Text, TextStyle } from "pixi.js";
+import { gsap } from "gsap";
+import { PixiPlugin, CSSPlugin } from "gsap/all";
+import { Container, Sprite, Text, TextStyle, Ticker } from "pixi.js";
 
 export class Scene extends Container {
   private readonly screenWidth: number;
   private readonly screenHeight: number;
 
   // We promoted clampy to a member of the class
+  private pyg: Sprite;
+  private animatedPyg: Sprite;
+
   private clampy: Sprite;
   private baseContainer: Container;
-  private pyg: Sprite;
+
   private textTest: Text;
   constructor(screenWidth: number, screenHeight: number) {
     super(); // Mandatory! This calls the superclass constructor.
 
+    gsap.registerPlugin(PixiPlugin, CSSPlugin);
     // see how members of the class need `this.`?
     this.screenWidth = screenWidth;
     this.screenHeight = screenHeight;
 
-    // Now clampy is a class member, we will be able to use it in another methods!
-    this.clampy = Sprite.from("clampy.png");
-
-    this.clampy.anchor.set(0.5);
-    this.clampy.x = this.screenWidth / 2;
-    this.clampy.y = this.screenHeight / 2;
-    this.addChild(this.clampy);
-
     this.baseContainer = new Container();
-    this.baseContainer.x = 200;
+    this.baseContainer.x = 0;
     this.baseContainer.y = 0;
-    this.addChild(this.baseContainer);
+
+    Ticker.shared.add(this.update, this);
+
+    console.log(gsap);
 
     const style = new TextStyle({
       align: "center",
@@ -36,17 +37,40 @@ export class Scene extends Container {
     });
     this.textTest = new Text("Wassup", style);
 
-    this.pyg = Sprite.from("pyg.png");
-    this.pyg.scale = { x: 0.1, y: 0.1 };
-
     this.clampy = Sprite.from("clampy.png");
     this.clampy.anchor.set(0.5);
     this.clampy.x = this.screenWidth / 2;
     this.clampy.y = this.screenHeight / 2;
 
+    this.pyg = Sprite.from("pyg.png");
+    this.pyg.anchor.set(0.5);
+    this.pyg.scale = { x: 0.11, y: 0.1 };
+    this.pyg.x = 200;
+
+    this.animatedPyg = Sprite.from("pyg.png");
+    this.animatedPyg.anchor.set(0.5);
+    this.animatedPyg.scale = { x: 0.11, y: 0.1 };
+    this.animatedPyg.x = -200;
+
+    const tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+    tl.to(this.animatedPyg, {
+      duration: 5,
+      x: 200,
+      rotation: 60,
+    });
+    tl.play();
+
+    console.log("PYG: ", tl);
+
     this.baseContainer.addChild(this.clampy);
+    this.clampy.addChild(this.animatedPyg);
     this.clampy.addChild(this.pyg);
 
     this.baseContainer.addChild(this.textTest);
+
+    this.addChild(this.baseContainer);
   }
+
+  private update(): void {}
 }
