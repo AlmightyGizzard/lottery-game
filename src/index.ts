e6650565175b6e457ce2cd84abd7a73b8d6712a9
Dominic (Laptop) <dominicohen+1@gmail.com>
2023-09-09 13:20:43 +0100

    Added new Image

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Sprite, Container } from "pixi.js";
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+  resolution: window.devicePixelRatio || 1,
+  autoDensity: true,
+  backgroundColor: 0x6495ed,
+  width: 640,
+  height: 480,
 });
 
+const conty: Container = new Container();
+conty.x = -200;
+conty.y = 0;
+app.stage.addChild(conty);
+
+const pyg: Sprite = Sprite.from("pyg.png");
+pyg.scale = { x: 0.1, y: 0.1 };
 const clampy: Sprite = Sprite.from("clampy.png");
 
 clampy.anchor.set(0.5);
@@ -16,4 +23,5 @@ clampy.anchor.set(0.5);
 clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
 
-app.stage.addChild(clampy);
+conty.addChild(clampy);
+clampy.addChild(pyg);
